31c11f641c5665bcc080bbdb2df0be687c2b8882
elenautkina <selenamondeu@gmail.com>
2023-08-08 18:08:46 +0300

    test_fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,11 +1,38 @@
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
+import pytest
+from selene import browser, have
 
 
-def test_github_desktop():
-    pass
+@pytest.fixture(scope='function', autouse=False,
+                params=[(1366, 768), (1920, 1080), (1280, 720)])
+def desktop_browser(request):
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    yield browser
+    browser.quit()
 
 
-def test_github_mobile():
-    pass
+@pytest.fixture(scope='function', autouse=False,
+                params=[(360, 640), (414, 896), (375, 667)])
+def mobile_browser(request):
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    yield browser
+    browser.quit()
+
+
+def test_github_desktop(desktop_browser):
+    browser.open('/')
+    browser.element("a[href='/login']").click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
+def test_github_mobile(mobile_browser):
+    browser.open('/')
+    browser.element('.flex-1 button').click()
+    browser.element("a[href='/login']").click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
